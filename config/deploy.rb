0c922236009db48d67bf360d610793c5f1666f9a
wfjeff <wm.jeffries@gmail.com>
2014-05-12 19:38:56 -0400

    add capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,30 @@
+require 'bundler/capistrano' # for bundler support
+
+set :application, "recommendsy"
+set :repository,  "git@github.com:recommendsy/recommendsy.git"
+
+set :user, 'flatiron'
+set :deploy_to, "/home/#{ user }/#{ application }"
+set :use_sudo, false
+
+set :scm, :git
+
+default_run_options[:pty] = true
+
+role :web, "107.170.136.214"                          # Your HTTP server, Apache/etc
+role :app, "107.170.136.214"                          # This may be the same as your `Web` server
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+ task :start do ; end
+ task :stop do ; end
+ task :restart, :roles => :app, :except => { :no_release => true } do
+   run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+ end
+end
