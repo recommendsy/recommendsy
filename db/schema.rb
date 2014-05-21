3e4336d6eddfc4dfab177ebaf807a7659d343d1f
wfjeff <wm.jeffries@gmail.com>
2014-05-21 18:11:29 -0400

    migrate to create reminders and user_reminders

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140519184427) do
+ActiveRecord::Schema.define(version: 20140521213405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,16 @@ ActiveRecord::Schema.define(version: 20140519184427) do
     t.datetime "updated_at"
     t.string   "listing_url"
     t.string   "images",      default: [], array: true
+  end
+
+  create_table "reminders", force: true do |t|
+    t.date    "reminder_date"
+    t.boolean "sent_status"
+  end
+
+  create_table "user_reminders", force: true do |t|
+    t.integer "user_id"
+    t.integer "reminder_id"
   end
 
   create_table "users", force: true do |t|
