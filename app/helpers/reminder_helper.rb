251ce2ff8c9583e20de26f27e82e22bf5c00793a
wfjeff <wm.jeffries@gmail.com>
2014-05-22 19:58:01 -0400

    add helper methods for calendar datepicker

--- app/helpers/reminder_helper.rb
+++ app/helpers/reminder_helper.rb
@@ -1,6 +1,14 @@
 module ReminderHelper
 
-  def pretty_format(reminder)
-    reminder.reminder_date.strftime('%Y/%m/%d')
+  def pretty_format(reminder, strf_code = '%Y/%m/%d')
+    reminder.reminder_date.strftime(strf_code)
+  end
+
+  def date_type(reminder)
+    if reminder.preset?
+      'preset-date'
+    else
+      'custom-date'
+    end
   end
 end
