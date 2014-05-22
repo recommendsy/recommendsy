5c18e7d2a38f1b8b0353c7122ef94125dc7a84ae
wfjeff <wm.jeffries@gmail.com>
2014-05-21 22:37:58 -0400

    add reminder controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'static#index'
 
+  #reminder routes
+  get 'reminders' => 'reminder#show'
+
   #user routes
   devise_for :users, :controllers => {:omniauth_callbacks => 'users/omniauth_callbacks'}
   devise_scope :user do
---
+++ spec/controllers/reminder_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe ReminderController do
+
+end
