68c26947ecec3b297470a93363d691f201ed03ad
Daniel Yang <yangsunwoo@gmail.com>
2014-05-22 23:56:24 -0400

    add update action for updating reminder active attribute

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -5,4 +5,14 @@ class RemindersController < ApplicationController
     @reminders = @user.reminders
   end
 
+  def update
+    @reminder = Reminder.find(params[:reminder_id])
+    @reminder.active = params[:active] == 'true' ? true : false
+    @reminder.save
+
+    respond_to do |format|
+      format.js {}
+    end
+  end
+
 end
